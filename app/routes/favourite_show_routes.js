0a8fdf41274e8696d4fc66384b4dfa8198299f6a
Rannar Allorg <rannar@rannars-mbp.int.radiolinja.ee>
2017-10-25 10:25:12 +0300

    Add get and post queries

--- app/routes/favourite_show_routes.js
+++ app/routes/favourite_show_routes.js
@@ -1,3 +1,20 @@
-module.exports = function (app, db) {
+var ObjectID = require('mongodb').ObjectID;
 
+module.exports = function (app, db) {
+    app.get('/favourites', (req, res) => {
+        db.collection('favourites').find().toArray((err, data) => {
+            res.send(data);
+        });
+    });
+
+    app.post('/favourites', (req, res) => {
+        const favourite = { text: req.body.showId, title: req.body.userId };
+        db.collection('favourites').insertOne(favourite, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
 const favouriteShowRoutes = require('./favourite_show_routes');
+const userRoutes = require('./user_routes');
 
 module.exports = function(app, db) {
     favouriteShowRoutes(app, db);
+    // userRoutes(app, db);
     // Other route groups could go here, in the future
 };
