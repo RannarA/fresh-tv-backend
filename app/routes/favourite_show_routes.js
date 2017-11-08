00aac3cefd6e13fe96c5024b80b80ea4c18515bc
rannar <rannarallorg@gmail.com>
2017-11-08 14:42:25 +0200

    Add auth to requests

--- app/routes/favourite_show_routes.js
+++ app/routes/favourite_show_routes.js
@@ -1,16 +1,32 @@
-var ObjectID = require('mongodb').ObjectID;
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+const secret = jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://rannara.eu.auth0.com/.well-known/jwks.json',
+});
+
+const jwtCheck = jwt({
+    secret: secret,
+    audience: '0qe3K92GFPuWTOIztKq30OwXgeif5JlH',
+    issuer: 'https://rannara.eu.auth0.com/',
+    algorithms: ['RS256'],
+});
 
 module.exports = function (app, db) {
+    app.use(jwtCheck);
+
     app.get('/favourites', (req, res) => {
-        db.collection('favourites').find().toArray((err, data) => {
+        console.log(req.user.sub)
+        db.collection('favourites').find({userId: req.user.sub}).toArray((err, data) => {
             res.send(data);
         });
     });
 
     app.post('/favourites', (req, res) => {
-        // TODO user
-        console.log(req.body)
-        const favourite = { showId: req.body.showId, userId: 1 };
+        const favourite = { showId: req.body.showId, userId: req.user.sub };
         db.collection('favourites').insertOne(favourite, (err, result) => {
             if (err) {
                 res.send({ 'error': 'An error has occurred' });
@@ -19,4 +35,18 @@ module.exports = function (app, db) {
             }
         });
     });
+
+    app.delete('/favourites/:show_id', (req, res) => {
+        const item = db.collection('favourites').findOne({showId: req.params.show_id,
+            userId: req.user.sub});
+        db.collection('favourites').removeOne({
+            '_id': item._id,
+        }, (err, result) => {
+            if (err)
+                result.send(err);
+
+            db.close();
+            // result.send({ message: 'Successfully deleted' });
+        });
+    })
 };
