1bad06f1e1e632555f1f615270f57341f4569f6f
rannar <rannarallorg@gmail.com>
2017-11-10 11:21:59 +0200

    Create guarded add tv show and get tv shows endpoints

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 const jwt = require('jsonwebtoken');
 const config = require('./config/config');
+
 const User = require('./app/models/user');
+const TvShow = require('./app/models/tvshow');
 
 const app = express();
 
@@ -21,96 +22,95 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-   res.send('Api works');
-});
-
-app.get('/setup', (req, res) => {
-   const nick = new User({
-       name: 'Nick Blah',
-       password: 'nickspass',
-       admin: true
-   });
-
-   nick.save(err => {
-       if (err) throw err;
-
-       console.log('User saved');
-       res.json({success: true});
-   })
+   res.send('Welcome to fresh-tv-backend');
 });
 
 const apiRoutes = express.Router();
 
+/*
+    AUTHENTICATION
+ */
 apiRoutes.post('/authenticate', (req, res) => {
-   User.findOne({
-       name: req.body.name
-   }, (err, user) => {
-       if (err) throw err;
+    User.findOne({
+        name: req.body.name
+    }, (err, user) => {
+        if (err) throw err;
 
-       if (!user) {
-           console.log('user not found');
-           res.json({success: false, message: 'Authentication failed'});
-       } else if (user) {
-           if (user.password !== req.body.password) {
-               console.log('Wrong password');
-               res.json({success: false, message: 'Authentication failed'});
-           } else {
-               const payload = {
-                   admin: user.admin
-               };
+        if (!user) {
+            console.log('user not found');
+            res.json({success: false, message: 'Authentication failed'});
+        } else if (user) {
+            if (user.password !== req.body.password) {
+                console.log('Wrong password');
+                res.json({success: false, message: 'Authentication failed'});
+            } else {
+                const payload = {
+                    admin: user.admin,
+                    userId: user._id
+                };
 
-               const token = jwt.sign(payload, app.get('jwtSecret'), {
-                   expiresIn: 1440
-               });
+                const token = jwt.sign(payload, app.get('jwtSecret'), {
+                    expiresIn: 1440
+                });
 
-               res.json({
-                   success: true,
-                   token: token
-               });
-           }
-       }
-   })
+                res.json({
+                    success: true,
+                    token: token
+                });
+            }
+        }
+    })
 });
 
+/*
+    Authentication guard
+ */
 apiRoutes.use((req, res, next) => {
-   const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    const token = req.headers['x-access-token'];
 
-   if (token) {
-       jwt.verify(token, app.get('jwtSecret'), (err, decoded) => {
-          if (err) {
-              return res.json({success: false, message: 'Authentication failed'});
-          } else {
-              req.decoded = decoded;
-              next();
-          }
-       });
-   } else {
-       res.status(403).send({
-           success: false,
-           message: 'Not authenticated'
-       });
-   }
+    if (token) {
+        jwt.verify(token, app.get('jwtSecret'), (err, decoded) => {
+            if (err) {
+                console.log(err)
+                return res.json({success: false, message: 'Authentication failed'});
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        res.status(403).send({
+            success: false,
+            message: 'Not authenticated'
+        });
+    }
 });
 
-apiRoutes.get('/', (req, res) => {
-    res.json({message: 'Api message'})
-});
-
-apiRoutes.get('/users', (req, res) => {
-    User.find({}, (err, users) => {
-        res.json(users);
+/*
+    Guarded endpoints
+ */
+apiRoutes.get('/watchlist', (req, res) => {
+    TvShow.find({userId: req.decoded.userId}, (err, tvShows) => {
+        res.json(tvShows);
     });
+});
+
+apiRoutes.post('/watchlist', (req, res) => {
+    const tvShow = new TvShow({
+        userId: req.decoded.userId,
+        showId: req.body.showId
+    });
+
+    tvShow.save(err => {
+        if (err) throw err;
+
+        res.status(200).send({
+            success: true
+        });
+    })
 });
 
 app.use('/api', apiRoutes);
 
 app.listen(port);
 console.log('It has begun');
-
-// MongoClient.connect(db.url, (err, database) => {
-//     if (err) return console.log(err);
-//     require('./app/routes')(app, database);
-//     app.listen(port, () => {
-//         console.log('We are live on ' + port);
-//     });
-// });
